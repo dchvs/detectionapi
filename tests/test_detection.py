237b0dde89833606dc59dfb0dcd2e6fa0cf13e9a
Daniel Chaves <danichg94@gmail.com>
2022-03-27 20:08:13 -0600

    detection: Add the preprocess stage unit test

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import unittest
 
 import cv2
@@ -7,9 +8,15 @@ import cv2
 from detectionapi.detection.detection import Detection
 
 
+example_input_image = os.path.dirname(
+    __file__) + '/../data/example_input_image.jpeg'
+
+
 class DetectionTests(unittest.TestCase):
     def setUp(self):
         self.this_detection_model = Detection()
-
-    def test_make(self):
         self.assertIsInstance(self.this_detection_model.net, cv2.dnn_Net)
+
+    def test_preprocess(self):
+        image = cv2.imread(example_input_image)
+        tensor = self.this_detection_model.preprocess(image)
